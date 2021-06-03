95ecff25222012edc26dd2cbcac1ed43d9664bf9
QaisAlshorman <>
2021-06-03 22:04:42 +0000

    modified on site

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
 
 alert('welcome to student site')
 
-var user = prompt("service you prefer?")
+var x = prompt("“What star rating would you give, 1-5?")
+
+while (true){
+if (x <1 )
+{
+  alert('you enter a un accepted value') 
+   var x = prompt ('renter star rating you would to give ,1-5') 
+ document.write('<https://img.freepik.com/free-photo/group-happy-young-students-university_85574-4531.jpg?size=626&ext=jpg" height="100px" width= "100px" />')
+}
+else if (x <= 5 )
+{
+document.write('<img src="https://img.freepik.com/free-photo/group-happy-young-students-university_85574-4531.jpg?size=626&ext=jpg" height="100px" width= "100px" />')
+break ;
+}
+
+
+
+  else if (x >5 ){ 
+     alert('you enter a un accepted value') 
+  x = prompt ('renter star rating you would to give ,1-5')
+
+document.write('<img src="https://img.freepik.com/free-photo/group-happy-young-students-university_85574-4531.jpg?size=626&ext=jpg" height="100px" width= "100px" />')
+
+  } 
+
+
+}
+
+
 
 alert('thank you to visit')
-
-confirm(" take your time")
