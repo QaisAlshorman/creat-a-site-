84f720cda25f6ef78e7a1d8d5317a3ec6d99edca
QaisAlshorman <>
2021-06-06 11:43:15 +0000

    modified

--- app.js
+++ app.js
@@ -3,25 +3,27 @@ alert('welcome to student site')
 
 var x = prompt("“What star rating would you give, 1-5?")
 
+function conditonal-stat {
 while (true){
 if (x <1 )
 {
   alert('you enter a un accepted value') 
    var x = prompt ('renter star rating you would to give ,1-5') 
-<<<<<<< HEAD
+
  document.write('<https://img.freepik.com/free-photo/group-happy-young-students-university_85574-4531.jpg?size=626&ext=jpg" height="100px" width= "100px" />')
 }
 else if (x <= 5 )
 {
 document.write('<img src="https://img.freepik.com/free-photo/group-happy-young-students-university_85574-4531.jpg?size=626&ext=jpg" height="100px" width= "100px" />')
-=======
+
  document.write('<img src="https://www.open.edu/openlearn/sites/www.open.edu.openlearn/files/ole_images/become_a_student_inline.jpg" height="100px" width= "100px" />')
 }
 else if (x <= 5 )
 {
 document.write('<img src="https://www.open.edu/openlearn/sites/www.open.edu.openlearn/files/ole_images/become_a_student_inline.jpg" height="100px" width= "100px" />')
->>>>>>> e0ecf46677f270bab9dccbf5035f5bd1224ad273
+
 break ;
+}
 }
 
 
@@ -30,17 +32,17 @@ break ;
      alert('you enter a un accepted value') 
   x = prompt ('renter star rating you would to give ,1-5')
 
-<<<<<<< HEAD
+
 document.write('<img src="https://img.freepik.com/free-photo/group-happy-young-students-university_85574-4531.jpg?size=626&ext=jpg" height="100px" width= "100px" />')
-=======
+
 document.write('<img src="https://www.open.edu/openlearn/sites/www.open.edu.openlearn/files/ole_images/become_a_student_inline.jpg" height="100px" width= "100px" />')
->>>>>>> e0ecf46677f270bab9dccbf5035f5bd1224ad273
+
 
   } 
 
 
 }
 
-
+conditonal-stat() ;
 
 alert('thank you to visit')
